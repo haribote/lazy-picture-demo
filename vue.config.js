bd57f545bbba06f7e756218ab286dac2183e9ba3
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-05-31 13:29:15 +0900

    add static html.

---
+++ vue.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          BASE_URL: '/'
+        },
+        template: `${__dirname}/public/static-image.html`,
+        filename: 'static-image.html'
+      }),
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          BASE_URL: '/'
+        },
+        template: `${__dirname}/public/external-svg.html`,
+        filename: 'external-svg.html'
+      }),
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          BASE_URL: '/'
+        },
+        template: `${__dirname}/public/embedded-svg.html`,
+        filename: 'embedded-svg.html'
+      })
+    ]
+  },
+
+  chainWebpack: config => {
+    /** CopyWebpackPlugin */
+    config
+      .plugin('copy')
+      .tap(args => {
+        args[0][0]
+          .ignore
+          .push(
+            'static-image.html',
+            'embedded-svg.html',
+            'embedded-svg.html'
+          )
+
+        return args
+      })
+  }
+}
