d838acf9bc331bf9de854348fef8233cdeae1e69
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-01 13:16:51 +0900

    add IntersectionObserver to LazyPicture.

--- src/components/LazyPicture.vue
+++ src/components/LazyPicture.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="picture" :class="classNames">
-    <img :src="src" :sizes="sizes" :alt="alt">
+    <img :src="currentSrc" :srcset="currentSrcSet" :sizes="sizes" :alt="alt">
   </div>
 </template>
 
 <script lang="ts">
+require('intersection-observer')
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
 export default class LazyPicture extends Vue {
+  @Prop({ type: IntersectionObserver })
+  observer = new IntersectionObserver(this.handleIntersection)
+
   @Prop({ required: true })
   placeholderSrc!: string
 
@@ -33,21 +37,55 @@ export default class LazyPicture extends Vue {
   @Prop({ default: false })
   isSquare!: boolean
 
-  src = ''
+  isLoaded = false
+
+  get currentSrc() {
+    return this.isLoaded ? this.alternativeSrc : this.placeholderSrc
+  }
+
+  get alternativeSrc() {
+    return this.srcset
+      .split(',')[0]
+      .replace(/^(.+\.(png|jpe?g|gif|webp)) +.+/, '$1')
+  }
+
+  get currentSrcSet() {
+    return this.isLoaded ? this.srcset : this.placeholderSrc
+  }
 
   get classNames() {
-    const { isBlur, isLandscape, isPortrait, isSquare } = this
+    const { isBlur, isLandscape, isPortrait, isSquare, isLoaded } = this
 
     return {
-      'is-blur': isBlur,
+      'is-blur': isBlur && !isLoaded,
       'is-landscape': isLandscape,
       'is-portrait': isPortrait,
       'is-square': isSquare
     }
   }
 
-  created() {
-    this.src = this.placeholderSrc
+  mounted() {
+    this.observer.observe(this.$el)
+  }
+
+  preloadImage() {
+    const { alternativeSrc, sizes, srcset } = this
+    const imgEl = new Image()
+
+    imgEl.onload = () => (this.isLoaded = true)
+
+    imgEl.sizes = sizes
+    imgEl.srcset = srcset
+    imgEl.src = alternativeSrc
+  }
+
+  handleIntersection(entries: IntersectionObserverEntry[]) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.observer.unobserve(this.$el)
+        this.preloadImage()
+      }
+    })
   }
 }
 </script>
