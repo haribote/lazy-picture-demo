666a243a9c37be17da78c5cc7e1967c70f6c27dd
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-04 12:02:21 +0900

    add LazyPictureProps interface.

---
+++ src/models/LazyPictureProps.ts
@@ -0,0 +1,10 @@
+export interface LazyPictureProps {
+  placeholderSrc: string
+  srcset: string
+  sizes: string
+  alt: string
+  isBlur: boolean
+  isLandscape: boolean
+  isPortrait: boolean
+  isSquare: boolean
+}
