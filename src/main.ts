d21bb45b1a2578314bb497b468ea5519a63d7869
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-04 12:05:21 +0900

    bind App to main.js.

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,50 @@
 import 'normalize.css'
+import App from './App.vue'
 import './assets/style.scss'
-import LazyPicture from './components/LazyPicture.vue'
+import { LazyPictureProps } from './models/LazyPictureProps'
 
-Array.from(document.querySelectorAll('.js-picture')).forEach(el => {
-  const imgEl = el.querySelector('img')
+function main() {
+  const wrapper = document.getElementById('app')
 
-  if (!imgEl) {
+  if (!wrapper) {
     return
   }
-  const placeholderSrc = imgEl.getAttribute('src')
-  const srcset = imgEl.dataset.srcset
-  const sizes = imgEl.sizes
-  const alt = imgEl.alt
-  const isBlur = el.classList.contains('is-blur')
-  const isLandscape = el.classList.contains('is-landscape')
-  const isPortrait = el.classList.contains('is-portrait')
-  const isSquare = el.classList.contains('is-square')
 
-  // tslint:disable-next-line:no-unused-expression
-  new LazyPicture({
-    el,
-    propsData: {
-      placeholderSrc,
-      srcset,
-      sizes,
-      alt,
-      isBlur,
-      isLandscape,
-      isPortrait,
-      isSquare
-    }
+  const pictures = Array.from(document.querySelectorAll('.js-picture'))
+    .map<LazyPictureProps | undefined>(el => {
+      const imgEl = el.querySelector('img')
+
+      if (!imgEl) {
+        return undefined
+      }
+
+      const placeholderSrc = imgEl.getAttribute('src') || ''
+      const srcset = imgEl.dataset.srcset || ''
+      const sizes = imgEl.sizes
+      const alt = imgEl.alt
+      const isBlur = el.classList.contains('is-blur')
+      const isLandscape = el.classList.contains('is-landscape')
+      const isPortrait = el.classList.contains('is-portrait')
+      const isSquare = el.classList.contains('is-square')
+
+      return {
+        placeholderSrc,
+        srcset,
+        sizes,
+        alt,
+        isBlur,
+        isLandscape,
+        isPortrait,
+        isSquare
+      }
+    })
+    .filter(picture => picture !== undefined)
+
+  const app = new App({
+    propsData: { pictures }
   })
-})
+
+  app.$mount(wrapper)
+}
+
+main()
